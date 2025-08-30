cf51f9fe2f7960ee6b9546ae4b144cd346f5b41d
Julliia-az <juliagabriellyazevedo46@gmail.com>
2025-08-29 22:33:35 -0300

    feat: adiciona o MenuBar na página 'Profile.jsx'

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -5,7 +5,11 @@ import bbColo from "./imagens/bbColo.jpg";
 import bbCozinhando from "./imagens/bbCozinhando.jpg";
 import bbEstudio from "./imagens/bbEstudio.jpg";
 import ultrassom from "./imagens/ultrassom.jpg";
-
+import { useNavigate } from "react-router-dom";
+import { Menubar } from "primereact/menubar";
+import { InputText } from "primereact/inputtext";
+import { Badge } from "primereact/badge";
+import { Avatar } from "primereact/avatar";
 import "./ProfilePage.css"; // Importa o CSS separado
 
 // Imagens
@@ -91,8 +95,74 @@ export default function Profile() {
     }
   };
 
+  const navigate = useNavigate();
+
+  const itemRenderer = (item) => (
+    <a className="flex align-items-center p-menuitem-link">
+      <span className={item.icon} />
+      <span className="mx-2">{item.label}</span>
+      {item.badge && <Badge className="ml-auto" value={item.badge} />}
+      {item.shortcut && (
+        <span className="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">
+          {item.shortcut}
+        </span>
+      )}
+    </a>
+  );
+  const items = [
+    {
+      //label: "Home",
+      icon: "pi pi-home",
+      command: () => navigate("/"),
+    },
+    {
+      //label: "User",
+      icon: "pi pi-user",
+      command: () => navigate("/perfil"),
+    },
+    {
+      //label: "Contact",
+      icon: "pi pi-bell",
+      badge: 1,
+      template: itemRenderer,
+    },
+  ];
+
+  const start = (
+    <img
+      alt="logo"
+      src="src/imagens/Logo.png"
+      height="45"
+      className="mr-2"
+    ></img>
+  );
+  const end = (
+    <div className="flex align-items-center gap-2">
+      <InputText
+        placeholder="Search"
+        type="text"
+        className="w-8rem sm:w-auto"
+        style={{
+          backgroundColor: "#cb9383",
+          borderRadius: "30",
+          height: "40px",
+          width: "200px",
+        }}
+      />
+      {/* <Avatar icon="pi pi-user" /> */}
+    </div>
+  );
+
   return (
     <div className="profile-page">
+      <div className="card">
+        <Menubar
+          model={items}
+          start={start}
+          end={end}
+          style={{ backgroundColor: "#c97d68" }}
+        />
+      </div>
       <div className="profile-container">
         <header className="profile-header">
           <div className="avatar-wrapper">
